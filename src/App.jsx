a98713b7d81cb3b30810158fd68aaeea8fe69433
Hyeok Lee <c235335@g.hongik.ac.kr>
2026-08-01 00:47:11 +0900

    chore: Router 설치 및 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
