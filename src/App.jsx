1c87d7a39c25a492c858b2e96e6fa89dd3a25064
yoonxxo <yoonxxo@gmail.com>
2026-08-02 01:23:06 +0900

    feat: 모달 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "@pages/HomePage";
+import ModalTestPage from "@pages/ModalTestPage";
 import StartPage from "@pages/StartPage";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/start" element={<StartPage />} />
+        <Route
+          path="/modal-test"
+          element={<ModalTestPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
